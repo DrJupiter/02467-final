c16ad1e5ef447d2ed9c4a72e2f852d339c934f65
DrJupiter <klausjupiter@hotmail.com>
2022-04-12 10:19:18 +0200

    n=50

--- twitter_data.py
+++ twitter_data.py
@@ -1,9 +1,42 @@
+# %%
 import yaml
-from twarc import Twarc
+import twarc
+from twarc.expansions import EXPANSIONS, USER_FIELDS, TWEET_FIELDS, PLACE_FIELDS, LIST_FIELDS
+import numpy as np
 
+# %%
 with open("./secret/keys.yaml", "r") as keyfile:
     keys = yaml.load(keyfile, yaml.FullLoader)
 
-t = Twarc(keys['ApiKey'], keys['ApiKeySecret'], keys['AccessToken'], keys['AccessTokenSecret'])
+t = twarc.Twarc2(keys['ApiKey'], keys['ApiKeySecret'], keys['AccessToken'], keys['AccessTokenSecret'], bearer_token = keys['BearerToken'])
+
+# %%
+path = './data/dehydrated/2022-02-27_3.csv'
+ids = np.loadtxt(path)
+ids = ids.astype(int)
+
+# %%
+
+tweets = t.tweet_lookup(ids)
+
+# %%
+data = list(tweets)
 
 
+# %%
+import json 
+
+# save data
+
+with open('./data/0227.json', 'w') as f:
+    json.dump(f, data) 
+
+# %%
+# load data again
+with open('./data/0227.json','r') as f:
+    data = json.load(f)
+
+
+# load file and then for each id create a csv file
+
+# %%
