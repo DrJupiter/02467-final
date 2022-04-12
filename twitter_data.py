66398f92987dd69cb1441a750e025dfd593662cc
DrJupiter <klausjupiter@hotmail.com>
2022-04-12 15:24:44 +0200

    n=50

--- twitter_data.py
+++ twitter_data.py
@@ -40,3 +40,25 @@ with open('./data/0227.json','r') as f:
 # load file and then for each id create a csv file
 
 # %%
+
+import os
+path = './data/dehydrated'
+dirs = [os.path.join(path,d) for d in os.listdir(path) if os.path.isdir(os.path.join(path,d))]
+
+
+for dir in dirs:
+    print(dir)
+    dirname = os.path.dirname(dir)
+    files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f))] 
+    print(files)
+    for file in files:
+        ids = np.loadtxt(os.path.join(dir,file))
+        ids = ids.astype(int)
+        #tweets = t.tweet_lookup(ids)
+        #data = list(tweets)
+        #with open(f'./data/hydrated/{dirname}/{file}.json','r') as f:
+        #    data = json.dump(data,f)
+
+    
+
+# %%
