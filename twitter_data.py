bf1e67431569f1db707a96bc7945e9d96c7fd16c
Rune-Nedergaard <rune7863@gmail.com>
2022-04-12 15:17:00 +0200

    Revert "123"
    This reverts commit 6d10944d4dc9b6e4888ee335801ce89983d1337a.

--- twitter_data.py
+++ twitter_data.py
@@ -1,5 +1,4 @@
 # %%
-
 import yaml
 import twarc
 from twarc.expansions import EXPANSIONS, USER_FIELDS, TWEET_FIELDS, PLACE_FIELDS, LIST_FIELDS
@@ -9,11 +8,10 @@ import numpy as np
 with open("./secret/keys.yaml", "r") as keyfile:
     keys = yaml.load(keyfile, yaml.FullLoader)
 
-t = twarc.Twarc2(keys['ApiKey'], keys['ApiKeySecret'], keys['AccessToken'], keys['AccessTokenSecret'],
-                 bearer_token=keys['BearerToken'])
+t = twarc.Twarc2(keys['ApiKey'], keys['ApiKeySecret'], keys['AccessToken'], keys['AccessTokenSecret'], bearer_token = keys['BearerToken'])
 
 # %%
-path = './data/dehydrated/2022-03-14_1.csv'
+path = './data/dehydrated/2022-02-27_3.csv'
 ids = np.loadtxt(path)
 ids = ids.astype(int)
 
@@ -24,18 +22,20 @@ tweets = t.tweet_lookup(ids)
 # %%
 data = list(tweets)
 
+
 # %%
-import json
+import json 
 
 # save data
 
 with open('./data/0227.json', 'w') as f:
-    json.dump(f, data)
+    json.dump(f, data) 
 
 # %%
 # load data again
-with open('./data/0227.json', 'r') as f:
+with open('./data/0227.json','r') as f:
     data = json.load(f)
+
 
 # load file and then for each id create a csv file
 
