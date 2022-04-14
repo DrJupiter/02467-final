1f5c6d0b3cb387525bfb61a049f738807104c3d2
Skroko <73649109+Skroko@users.noreply.github.com>
2022-04-14 10:42:07 +0200

    Update count_langs.py

--- count_langs.py
+++ count_langs.py
@@ -1,24 +1,47 @@
 #%%
+from xml.etree.ElementInclude import default_loader
 import pandas as pd
 import numpy as np
 import json
 import os
-
+from collections import defaultdict
+import re
 #%%
 
 
 path = './data/hydrated/data/dehydrated'
 
-# dirs = [os.path.join(path,d) for d in os.listdir(path) if os.path.isdir(os.path.join(path,d))]
+lang_days = defaultdict(lambda: defaultdict(int))
 
-# dirs
+c = 0
+
 for filename in os.listdir(path):
+    # print("filenam",filename)
     f = os.path.join(path, filename)
     # checking if it is a file
     if os.path.isfile(f):
-        print(f)
         with open(f,'r') as fi:
             data = json.load(fi)
 
-        pd.DataFrame(data)    
-    break
+        day = re.findall(f"[0-9-]+",f)[0]
+        if day not in lang_days:
+            lang_days[day] = defaultdict(int)
+            print("added day",day)
+
+
+        for i in range(len(data)):
+            for j in range(len(data[i]["data"])):
+                lang = data[i]["data"][j]["lang"]
+                lang_days[day][lang] += 1
+
+        # print(df.iloc[0]["data"][0]["context_annotations"])
+    
+
+#%%
+
+lang_days["2022-03-12"]["uk"]
+
+
+#%%
+import matplotlib.pyplot
+
