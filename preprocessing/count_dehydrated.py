faeaf6ce7762b618da98966fd3b545a4ca1770bb
Rune-Nedergaard <rune7863@gmail.com>
2022-05-02 14:44:45 +0200

    added count func

---
+++ preprocessing/count_dehydrated.py
@@ -0,0 +1,23 @@
+
+import os
+
+import matplotlib.pyplot as plt
+from glob import glob
+from pathlib import Path
+
+code_path = Path(os.getcwd())
+path = code_path.joinpath('./data/complete/')
+
+def count_dehydrated(path):
+
+    subfolders = [ f.path for f in os.scandir(path) if f.is_dir() ]
+
+    total = []
+    for path in subfolders:
+        total.append(0)
+        for filename in os.listdir(path):
+            with open(path+ "\\" + filename, 'r', encoding="latin-1") as fileObj:
+                # -1 to exclude the header
+                total[-1] += len(fileObj.readlines()) #not -1 since no header
+                #print("Rows Counted {} in the csv {}:".format(len(fileObj.readlines()) - 1, filename))  
+    return total
